1d6efd6a924c92d6032db3b1011c2443dbc40540
Elias Oliveira <ti.eliasoliveira@gmail.com>
2022-08-30 08:55:18 -0300

    ajuste arquivo de configuração do banco

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
     define('HOST', 'localhost');
     define('USER', 'root');
     define('PASS', '');
-    define('BASE', 'test');
+    define('BASE', 'calculo_imc');
 
     $conn = new mysqli(HOST,USER,PASS,BASE);
