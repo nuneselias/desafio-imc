a59c508844b8cc704ccdf1a307c2b062d8fbf26b
Elias Oliveira <ti.eliasoliveira@gmail.com>
2022-08-30 08:28:43 -0300

    estrutura html e conexão com o banco

---
+++ home.php
@@ -0,0 +1,14 @@
+<section id="main-home">
+    <div id="content-left">
+        <div id="content-center">
+            <h1 class="display-3 text-light bg-dark">Calculadora IMC</h1>
+            <div class="card" style="width: 25rem;">
+                <div class="card-body">
+                    <h2 class="card-title display-6">Menu</h2>
+                    <a id="btn-iniciar" href="?page=novo" class="btn btn-outline-primary btn-lg" role="button">Novo Usuário</a>
+                    <a href="?page=listar " class="btn btn-outline-primary btn-lg" role="button">Listar Usuários</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>IMC - Calculadora</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col mt-5">
+                <?php
+                include("config.php");
+                switch (@$_REQUEST["page"]) {
+                    case "novo":
+                        include("novo-usuario.php");
+                        break;
+                    case "listar":
+                        include("listar-usuario.php");
+                        break;
+                    case "salvar":
+                        include("salvar-usuario.php");
+                        break;
+                    case "editar":
+                        include("editar-usuario.php");
+                        break;
+                    case "calculo":
+                        include("calculo-usuario.php");
+                        break;
+                    default:
+                        include("home.php");
+                    }
+                ?>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
